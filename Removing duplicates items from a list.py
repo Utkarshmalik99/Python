73d409a1477f2b92d5719ff9b95cae50b55cfa9e
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-11 23:13:46 +0530

    Update Removing duplicates items from a list.py

--- Removing duplicates items from a list.py
+++ Removing duplicates items from a list.py
@@ -3,3 +3,7 @@ print("Original= ", listNumbers)
 
 listNumbers = list(set(listNumbers))
 print("After removing duplicate= ", listNumbers)
+
+#Output-
+#Original=  [20, 22, 24, 26, 28, 28, 20, 30, 24]
+#After removing duplicate=  [20, 22, 24, 26, 28, 30]
