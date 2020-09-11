1b96a6503a1af7a01853d1552d8eaf0d62a4be86
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-11 23:22:17 +0530

    Create Program to access the list element and list indices.py

---
+++ Program to access the list element and list indices.py
@@ -0,0 +1,11 @@
+listOne = [123, 345, 456, 23]
+print("Using enumerate")
+for index, element in enumerate(listOne): 
+    print("Index [", index,"]", "Value", element)
+    
+# Output-
+# Using enumerate
+# Index [ 0 ] Value 123
+# Index [ 1 ] Value 345
+# Index [ 2 ] Value 456
+# Index [ 3 ] Value 23
