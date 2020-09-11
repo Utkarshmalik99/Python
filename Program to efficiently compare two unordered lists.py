0633f9d79e1d16f8d03e4122ff3c55840b8db1b3
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-11 23:10:45 +0530

    Create Program to efficiently compare two unordered lists.py

---
+++ Program to efficiently compare two unordered lists.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+one = [33, 22, 11, 44, 55]
+two = [22, 11, 44, 55, 33]
+
+print("Are two list equal?",Counter(one) == Counter(two))
+
+#Output-Are two list equal? True
