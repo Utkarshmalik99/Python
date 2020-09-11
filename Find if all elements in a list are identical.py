6088fbcb7544332452e02ae92f4a0f127b2aca11
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-11 23:14:42 +0530

    Update Find if all elements in a list are identical.py

--- Find if all elements in a list are identical.py
+++ Find if all elements in a list are identical.py
@@ -3,3 +3,7 @@ print("All element are duplicate in listOne:", listOne.count(listOne[0]) == len(
 
 listTwo = [20, 20, 20, 50]
 print("All element are duplicate in listTwo:", listTwo.count(listTwo[0]) == len(listTwo))
+
+#Output-
+#All element are duplicate in listOne: True
+#All element are duplicate in listTwo: False
