cc9b29dd2910836b1f1a2e342501ba095fffc7dd
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-23 22:32:10 +0530

    Update ASCII_Value_of_character.py

--- ASCII_Value_of_character.py
+++ ASCII_Value_of_character.py
@@ -1,2 +1,2 @@
-a=str(input("enter the character:"))
-print("The ASCII Value of entered character is",ord(a))
+a=str(input("enter the character: "))
+print("The ASCII Value of entered character is ",ord(a))
